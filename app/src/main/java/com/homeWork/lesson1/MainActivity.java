cbf22755eabff8158a9ea1783827aeaac5a484db
Raman Yarotski <rvlogist@gmail.com>
2020-10-25 17:08:51 +0300

    Home work 1. Update gradle version to 6.5

--- app/src/main/java/com/homeWork/lesson1/MainActivity.java
+++ app/src/main/java/com/homeWork/lesson1/MainActivity.java
@@ -1,8 +1,8 @@
 package com.homeWork.lesson1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
