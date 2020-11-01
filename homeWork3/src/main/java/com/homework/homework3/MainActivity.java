58e338c8f79c084e4bc9733e3ea2862c532a36e0
Raman Yarotski <rvlogist@gmail.com>
2020-11-02 00:50:30 +0300

    HW3 Task 1.2

--- homeWork3/src/main/java/com/homework/homework3/MainActivity.java
+++ homeWork3/src/main/java/com/homework/homework3/MainActivity.java
@@ -1,14 +1,15 @@
 package com.homework.homework3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_poll_linear);
+        setContentView(R.layout.activity_poll_table);
     }
+
 }
