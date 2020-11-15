c7ff9049f78c188a3804659114884adcc9100822
Raman Yarotski <rvlogist@gmail.com>
2020-11-15 16:57:33 +0300

    HW3 Task 3.2

--- homeWork3/src/main/java/com/homework/homework3/MainActivity.java
+++ homeWork3/src/main/java/com/homework/homework3/MainActivity.java
@@ -9,7 +9,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in_linear);
+        setContentView(R.layout.activity_sign_in_constraint);
     }
 
+//      Landscape and portrait:
+//  activity_photo_constraint
+//  activity_photo_linear
+//  activity_poll_linear
+//  activity_poll_table
+//  activity_sign_in_linear
+//  activity_sign_in_constraint
+
+//      Only portrait:
+//  activity_photo_relative
 }
